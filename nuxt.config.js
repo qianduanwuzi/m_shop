5fa0fa3db9efbdc37bfa9a91a34f020b5c16dcad
wuzi <549243104@qq.com>
2018-03-07 11:15:55 +0800

    修复切换路由meta-scale初始化

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,14 @@ module.exports = {
     title: 'test-nuxt',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+       { src: '/msuit.js' }
     ]
   },
   /*
