be32ecece31317501977a1dea1d74b9a8a27acd9
wuzi <549243104@qq.com>
2018-03-13 09:11:07 +0800

    no bottom menu

---
+++ layouts/noBottomMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="content">
+    <!-- 无底部导航 -->
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  // use way
+  // layout: 'noBottomMenu',
+    components: {
+    },
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
